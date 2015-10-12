ca8c233b7018435b368aec66d61066fe577dc8cb
edom18 <edo.m18@gmail.com>
2015-10-12 11:11:11 +0900

    removed a comment.

--- EdoUtilityTests/EdoUtilityTests.m
+++ EdoUtilityTests/EdoUtilityTests.m
@@ -1,10 +1,3 @@
-//
-//  EdoUtilityTests.m
-//  EdoUtilityTests
-//
-//  Created by 比留間 和也 on 2014/12/07.
-//  Copyright (c) 2014年 比留間 和也. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
